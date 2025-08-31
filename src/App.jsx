4aecdb2562aac2c831d8b6e66d5f49a61cfafeb5
GeorgesLevyEtoundi <etoundioyono.georeslevy@gmail.com>
2025-08-31 10:25:04 +0000

    Display coins data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import CoinCard from './components/CoinCard';
+const API_URL =
+	'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&spqrkline=false';
+
 const App = () => {
-	return <>Crypto Dash App</>;
+	const [coins, setCoins] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchCoins = async () => {
+			try {
+				const res = await fetch(API_URL);
+
+				if (!res.ok) throw new Error('Failed to fetch data');
+
+				const data = await res.json();
+
+				console.log(data);
+
+				setCoins(data);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchCoins();
+	}, []);
+
+	return (
+		<div>
+			{loading && <p>Loading...</p>}
+
+			{error && <div className="error"> {error} </div>}
+
+			{!loading && !error && (
+				<div>
+					<main className="grid">
+						{coins.map(coin => (
+							<CoinCard
+								key={coin.id}
+								coin={coin}
+							/>
+						))}
+					</main>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default App;
