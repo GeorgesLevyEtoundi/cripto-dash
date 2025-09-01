a0a2f769384213262fa99f32205914c04f9c0b0f
GeorgesLevyEtoundi <etoundioyono.georeslevy@gmail.com>
2025-09-01 15:02:03 +0000

    Modified project structure. Added pages folder to project and created home.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router';
 
 // components
-import CoinCard from './components/CoinCard';
-import LimitSelector from './components/LimitSelector';
-import FilterInput from './components/FilterInput';
-import SortSelector from './components/SortSelector';
+import HomePage from './pages/home';
 
 // env variables
 const API_URL = import.meta.env.VITE_API_URL;
@@ -39,76 +37,25 @@ const App = () => {
 		fetchCoins();
 	}, [limit]);
 
-	const filteredCoins = coins
-		.filter(coin => {
-			return (
-				coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-				coin.symbol.toLowerCase().includes(filter.toLowerCase())
-			);
-		})
-		.slice() // 🔥 Important: make a shallow copy before sorting!
-		.sort((a, b) => {
-			switch (sortBy) {
-				case 'market_cap_desc':
-					return b.market_cap - a.market_cap;
-				case 'price_desc':
-					return b.current_price - a.current_price;
-				case 'price_asc':
-					return a.current_price - b.current_price;
-				case 'change_desc':
-					return (
-						b.price_change_percentage_24h -
-						a.price_change_percentage_24h
-					);
-				case 'change_asc':
-					return (
-						a.price_change_percentage_24h -
-						b.price_change_percentage_24h
-					);
-				default:
-					return 0;
-			}
-		});
-
 	return (
-		<div>
-			<h1>Crypto Dash</h1>
-			{loading && <p>Loading...</p>}
-
-			{error && <div className="error"> {error} </div>}
-
-			<div className="top-controls">
-				<FilterInput
-					filter={filter}
-					setFilter={setFilter}
-				/>
-				<SortSelector
-					sortBy={sortBy}
-					setSortBy={setSortBy}
-				/>
-				<LimitSelector
-					limit={limit}
-					setLimit={setLimit}
-				/>
-			</div>
-
-			{!loading && !error && (
-				<div>
-					<main className="grid">
-						{filteredCoins.length > 0 ? (
-							filteredCoins.map(coin => (
-								<CoinCard
-									key={coin.id}
-									coin={coin}
-								/>
-							))
-						) : (
-							<p>No matching coins</p>
-						)}
-					</main>
-				</div>
-			)}
-		</div>
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<HomePage
+						coins={coins}
+						filter={filter}
+						setFilter={setFilter}
+						limit={limit}
+						setLimit={setLimit}
+						sortBy={sortBy}
+						setSortBy={setSortBy}
+						loading={loading}
+						error={error}
+					/>
+				}
+			/>
+		</Routes>
 	);
 };
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,91 @@
+// components
+import CoinCard from '../components/CoinCard';
+import LimitSelector from '../components/LimitSelector';
+import FilterInput from '../components/FilterInput';
+import SortSelector from '../components/SortSelector';
+
+const HomePage = ({
+	coins,
+	filter,
+	setFilter,
+	limit,
+	setLimit,
+	sortBy,
+	setSortBy,
+	loading,
+	error,
+}) => {
+	const filteredCoins = coins
+		.filter(coin => {
+			return (
+				coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+				coin.symbol.toLowerCase().includes(filter.toLowerCase())
+			);
+		})
+		.slice() // 🔥 Important: make a shallow copy before sorting!
+		.sort((a, b) => {
+			switch (sortBy) {
+				case 'market_cap_desc':
+					return b.market_cap - a.market_cap;
+				case 'price_desc':
+					return b.current_price - a.current_price;
+				case 'price_asc':
+					return a.current_price - b.current_price;
+				case 'change_desc':
+					return (
+						b.price_change_percentage_24h -
+						a.price_change_percentage_24h
+					);
+				case 'change_asc':
+					return (
+						a.price_change_percentage_24h -
+						b.price_change_percentage_24h
+					);
+				default:
+					return 0;
+			}
+		});
+
+	return (
+		<div>
+			<h1>Crypto Dash</h1>
+			{loading && <p>Loading...</p>}
+
+			{error && <div className="error"> {error} </div>}
+
+			<div className="top-controls">
+				<FilterInput
+					filter={filter}
+					setFilter={setFilter}
+				/>
+				<SortSelector
+					sortBy={sortBy}
+					setSortBy={setSortBy}
+				/>
+				<LimitSelector
+					limit={limit}
+					setLimit={setLimit}
+				/>
+			</div>
+
+			{!loading && !error && (
+				<div>
+					<main className="grid">
+						{filteredCoins.length > 0 ? (
+							filteredCoins.map(coin => (
+								<CoinCard
+									key={coin.id}
+									coin={coin}
+								/>
+							))
+						) : (
+							<p>No matching coins</p>
+						)}
+					</main>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default HomePage;
